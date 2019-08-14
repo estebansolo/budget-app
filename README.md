5d6217b3e85fec836d1500f2989c9fb981586666
Esteban Solorzano <david.solorzano@atlanticsoft.co>
2019-08-14 16:41:38 -0500

    Setup production webpack build

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # Budget App
+
+ - `git init` Create a new git repo
+ - `git status` - View the changes to your project code
+ - `git add` - Add files to staging area
+ - `git commit` - Create a new commit with files from staging area
+ - `git log` - View recent commits
--- webpack.config.js
+++ webpack.config.js
@@ -1,41 +1,65 @@
 // entry -> output
-
 const path = require('path');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
-module.exports = {
-	entry: './src/app.js',
-	//entry: './src/playground/hoc.js',
-	output: {
-		path: path.join(__dirname, 'public', 'scripts'),
-		filename: 'bundle.js'
-	},
-	module: {
-		rules: [
-			{
-				test: /\.js$/,
-				exclude: /node_modules/,
-				// loaders:[
-				//     'babel-loader'
-				// ]
-				use: [
-					{
-						loader: 'babel-loader'
-					}
-				]
-			},
-			{
-				test: /\.s?css$/,
-				use: ['style-loader', 'css-loader', 'sass-loader']
-			}
-		]
-	},
-	mode: 'development',
-	devtool: 'cheap-module-eval-source-map',
-	devServer: {
-		contentBase: path.join(__dirname, 'public'),
-		publicPath: '/scripts/',
-		historyApiFallback: true
-	}
+module.exports = (env, argv) => {
+	const isProduction = env === 'production';
+	const CSSExtract = new MiniCssExtractPlugin({
+		// Options similar to the same options in webpackOptions.output
+		// all options are optional
+		filename: 'styles.css',
+		ignoreOrder: false // Enable to remove warnings about conflicting order
+	});
+
+	return {
+		entry: './src/app.js',
+		//entry: './src/playground/hoc.js',
+		output: {
+			path: path.join(__dirname, 'public', 'scripts'),
+			filename: 'bundle.js'
+		},
+		module: {
+			rules: [
+				{
+					test: /\.js$/,
+					exclude: /node_modules/,
+					// loaders:[
+					//     'babel-loader'
+					// ]
+					use: [
+						{
+							loader: 'babel-loader'
+						}
+					]
+				},
+				{
+					test: /\.s?css$/,
+					use: [
+						MiniCssExtractPlugin.loader,
+						{
+							loader: 'css-loader',
+							options: {
+								sourceMap: true
+							}
+						},
+						{
+							loader: 'sass-loader',
+							options: {
+								sourceMap: true
+							}
+						}
+					]
+				}
+			]
+		},
+		plugins: [ CSSExtract ],
+		devtool: isProduction ? 'source-map' : 'inline-source-map',
+		devServer: {
+			contentBase: path.join(__dirname, 'public'),
+			publicPath: '/scripts/',
+			historyApiFallback: true
+		}
+	};
 };
 
 // Loader
